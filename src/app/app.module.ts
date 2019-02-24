a878290c4d81389185cdd2b2b38032422282f534
Tetiana Platonova <malyfko@gmail.com>
2019-02-24 11:04:45 +0100

    Initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BackgroundColorDirective } from './background-color.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundColorDirective
   ],
   imports: [
     BrowserModule,
